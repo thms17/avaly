426df2df0e151d908d6fd028026291bce3a71c15
Thomas <46416867+thms17@users.noreply.github.com>
2025-07-05 14:10:32 +0200

    sticky section fertig

--- src/components/sticky-solution.js
+++ src/components/sticky-solution.js
@@ -9,9 +9,9 @@ const solutionImages = gsap.utils.toArray('.solution-image');
 if (scrollContainer && solutionImages.length > 0) {
   // Manuelle Definition der Scrollabschnitte
   const scrollSteps = [
-    { startPercent: 0, endPercent: 15 },
-    { startPercent: 16, endPercent: 32 },
-    { startPercent: 33, endPercent: 49 },
+    { startPercent: 0, endPercent: 10 },
+    { startPercent: 11, endPercent: 37 },
+    { startPercent: 38, endPercent: 48 },
   ];
 
   // Initiale Klassenzuordnung
@@ -31,12 +31,22 @@ if (scrollContainer && solutionImages.length > 0) {
       end: `${step.endPercent}% top`,
       onEnter: () => {
         solutionImages.forEach((img) => img.classList.remove('is-active'));
+        document
+          .querySelectorAll('.solution_text-step')
+          .forEach((step) => step.classList.remove('is-active'));
         el.classList.add('is-active');
+        const textSteps = gsap.utils.toArray('.solution_text-step');
+        if (textSteps[i]) textSteps[i].classList.add('is-active');
         console.log(`→ is-active gesetzt bei .is-${i + 1}`);
       },
       onEnterBack: () => {
         solutionImages.forEach((img) => img.classList.remove('is-active'));
+        document
+          .querySelectorAll('.solution_text-step')
+          .forEach((step) => step.classList.remove('is-active'));
         el.classList.add('is-active');
+        const textSteps = gsap.utils.toArray('.solution_text-step');
+        if (textSteps[i]) textSteps[i].classList.add('is-active');
         console.log(`← is-active zurückgesetzt bei .is-${i + 1}`);
       },
       markers: true,
